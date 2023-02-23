46f8b5fa2383e76c49639516689298b47da39889
JhsK <qwe6293@nate.com>
2023-02-23 21:50:41 +0900

    feat: markdown parsing server component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,7 @@
-import './globals.css'
+"use client";
+import { Container, Nav, Navbar, NavDropdown } from "react-bootstrap"
+import 'bootstrap/dist/css/bootstrap.min.css';
+import layout from './layout.module.css';
 
 export default function RootLayout({
   children,
@@ -6,13 +9,38 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
+    <html lang="ko">
       {/*
         <head /> will contain the components returned by the nearest parent
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
       <head />
-      <body>{children}</body>
+      <body>
+        <Navbar bg="bg-white" expand="lg">
+      <Container>
+        <Navbar.Brand href="/" className="title">Sungkyu</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#link">Link</Nav.Link>
+            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">
+                Another action
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">
+                Separated link
+              </NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+      {/* {children} */}
+      </body>
     </html>
   )
 }
