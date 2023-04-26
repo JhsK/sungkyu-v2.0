9b2f03a2063587856f77de38eee2037def1736db
JhsK <qwe6293@nate.com>
2023-04-26 14:17:34 +0900

    chore: 13.3 version update & tailwind install

--- next.config.js
+++ next.config.js
@@ -1,14 +1,8 @@
 /** @type {import('next').NextConfig} */
-const path = require('path');
-
 const nextConfig = {
   experimental: {
     appDir: true,
   },
-  sassOptions: {
-    includePaths: [path.join(__dirname, 'styles')],
-  },
 }
 
 module.exports = nextConfig
-
