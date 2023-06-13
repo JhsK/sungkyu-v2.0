f86cd32cc5e4a6cf8fbdeff1fc2466dc9fac091c
JhsK <qwe6293@nate.com>
2023-06-13 21:39:54 +0900

    feat: nav value change & position add

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -13,11 +13,8 @@ const user = {
     'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
 };
 const navigation = [
-  { name: 'Dashboard', href: '#', current: true },
-  { name: 'Team', href: '#', current: false },
-  { name: 'Projects', href: '#', current: false },
-  { name: 'Calendar', href: '#', current: false },
-  { name: 'Reports', href: '#', current: false },
+  { name: '블로그', href: '/', current: true },
+  { name: '소개', href: '#', current: false },
 ];
 const userNavigation = [
   { name: 'Your Profile', href: '#' },
@@ -28,7 +25,7 @@ const userNavigation = [
 const Nav = () => {
   return (
     <>
-      <div className="min-h-full">
+      <div className="min-h-full sticky top-0 w-full z-10">
         <Disclosure as="nav">
           {({ open }) => (
             <>
@@ -89,7 +86,24 @@ const Nav = () => {
                             />
                           </Menu.Button>
                         </div>
-                        <Transition
+                        <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                          {userNavigation.map((item) => (
+                            <Menu.Item key={item.name}>
+                              {({ active }) => (
+                                <a
+                                  href={item.href}
+                                  className={classNames(
+                                    active ? 'bg-gray-100' : '',
+                                    'block px-4 py-2 text-sm text-gray-700'
+                                  )}
+                                >
+                                  {item.name}
+                                </a>
+                              )}
+                            </Menu.Item>
+                          ))}
+                        </Menu.Items>
+                        {/* <Transition
                           as={Fragment}
                           enter="transition ease-out duration-100"
                           enterFrom="transform opacity-0 scale-95"
@@ -115,13 +129,12 @@ const Nav = () => {
                               </Menu.Item>
                             ))}
                           </Menu.Items>
-                        </Transition>
+                        </Transition> */}
                       </Menu>
                     </div>
                   </div>
                   <div className="-mr-2 flex md:hidden">
-                    {/* Mobile menu button */}
-                    <Disclosure.Button className="inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:text-gray-400 focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
+                    <Disclosure.Button className="inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:text-gray-400">
                       <span className="sr-only">Open main menu</span>
                       {open ? (
                         <XMarkIcon
