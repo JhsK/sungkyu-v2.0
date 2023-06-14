b14cd842308fdb62e9600ea5a3e4034cc3d45039
JhsK <qwe6293@nate.com>
2023-06-14 21:28:26 +0900

    feat: nav desktop ui fix & icon add

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -5,6 +5,7 @@ import { Disclosure, Menu, Transition } from '@headlessui/react';
 import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline';
 import classNames from 'classnames';
 import Link from 'next/link';
+import { RxGithubLogo, RxLinkedinLogo } from 'react-icons/rx';
 
 const user = {
   name: 'Tom Cook',
@@ -46,7 +47,7 @@ const Nav = () => {
                         />
                       </Link>
                     </div>
-                    <div className="hidden md:block">
+                    {/* <div className="hidden md:block">
                       <div className="ml-10 flex items-baseline space-x-4">
                         {navigation.map((item) => (
                           <a
@@ -64,73 +65,41 @@ const Nav = () => {
                           </a>
                         ))}
                       </div>
-                    </div>
+                    </div> */}
                   </div>
-                  <div className="hidden md:block">
-                    <div className="ml-4 flex items-center md:ml-6">
-                      <button
-                        type="button"
-                        className="rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
-                      >
-                        <span className="sr-only">View notifications</span>
-                        <BellIcon className="h-6 w-6" aria-hidden="true" />
-                      </button>
-                      <Menu as="div" className="relative ml-3">
-                        <div>
-                          <Menu.Button className="flex max-w-xs items-center rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
-                            <span className="sr-only">Open user menu</span>
-                            <img
-                              className="h-8 w-8 rounded-full"
-                              src={user.imageUrl}
-                              alt=""
-                            />
-                          </Menu.Button>
-                        </div>
-                        <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-                          {userNavigation.map((item) => (
-                            <Menu.Item key={item.name}>
-                              {({ active }) => (
-                                <a
-                                  href={item.href}
-                                  className={classNames(
-                                    active ? 'bg-gray-100' : '',
-                                    'block px-4 py-2 text-sm text-gray-700'
-                                  )}
-                                >
-                                  {item.name}
-                                </a>
-                              )}
-                            </Menu.Item>
-                          ))}
-                        </Menu.Items>
-                        {/* <Transition
-                          as={Fragment}
-                          enter="transition ease-out duration-100"
-                          enterFrom="transform opacity-0 scale-95"
-                          enterTo="transform opacity-100 scale-100"
-                          leave="transition ease-in duration-75"
-                          leaveFrom="transform opacity-100 scale-100"
-                          leaveTo="transform opacity-0 scale-95"
+                  <div className="hidden md:flex">
+                    <div className="flex items-center space-x-4">
+                      {navigation.map((item) => (
+                        <a
+                          key={item.name}
+                          href={item.href}
+                          className="text-base"
+                          // className={classNames(
+                          //   item.current
+                          //     ? 'bg-gray-900 text-white'
+                          //     : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                          //   'rounded-md px-3 py-2 text-sm font-medium'
+                          // )}
+                          aria-current={item.current ? 'page' : undefined}
                         >
-                          <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-                            {userNavigation.map((item) => (
-                              <Menu.Item key={item.name}>
-                                {({ active }) => (
-                                  <a
-                                    href={item.href}
-                                    className={classNames(
-                                      active ? 'bg-gray-100' : '',
-                                      'block px-4 py-2 text-sm text-gray-700'
-                                    )}
-                                  >
-                                    {item.name}
-                                  </a>
-                                )}
-                              </Menu.Item>
-                            ))}
-                          </Menu.Items>
-                        </Transition> */}
-                      </Menu>
+                          {item.name}
+                        </a>
+                      ))}
+                    </div>
+                    <div className="mx-4 mt-0.5 opacity-20 font-thin">|</div>
+                    <div className="flex">
+                      <Link href="https://github.com/JhsK" target="_blank">
+                        <RxGithubLogo
+                          size={24}
+                          className="flex cursor-pointer items-center opacity-50 mr-4 hover:opacity-100"
+                        />
+                      </Link>
+                      <Link href="https://www.linkedin.com/in/%EC%84%B1%EA%B7%9C-%EC%9E%84-5b2b8b23a/">
+                        <RxLinkedinLogo
+                          size={24}
+                          className="flex cursor-pointer items-center opacity-50 hover:opacity-100"
+                        />
+                      </Link>
                     </div>
                   </div>
                   <div className="-mr-2 flex md:hidden">
