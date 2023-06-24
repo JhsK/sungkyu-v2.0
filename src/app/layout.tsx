9221b9361d6f0466daeb2c3cf873c10670999f78
JhsK <qwe6293@nate.com>
2023-06-24 16:42:44 +0900

    feat: google analytices script add

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import { Nav } from '@/components/Nav';
 import ThemeProvider from '@/components/ThemeProvider';
+import Script from 'next/script';
 
 export const metadata = {
   title: 'Sungkyu blog',
@@ -13,6 +14,22 @@ export default function RootLayout({
 }) {
   return (
     <html lang="ko">
+      <Script
+        strategy="afterInteractive"
+        src="https://www.googletagmanager.com/gtag/js?id=G-2X49GH91GC"
+      />
+      <Script
+        id="gtag-init"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-2X49GH91GC');
+          `,
+        }}
+      />
       <body>
         <ThemeProvider>
           <Nav />
