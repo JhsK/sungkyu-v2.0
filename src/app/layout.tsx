a01e702c3339aab315ae0add705abe4f3bfc00ba
JhsK <qwe6293@nate.com>
2023-06-30 19:27:28 +0900

    chore: ga4 script span

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -31,6 +31,7 @@ export default function RootLayout({
           window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
           gtag('js', new Date());
+
           gtag('config', 'G-825XQ2WCW4');
           `,
         }}
