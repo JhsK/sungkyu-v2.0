cc4f9d0e381768cc1c56ea743f683f9b4289b8f9
JhsK <qwe6293@nate.com>
2023-07-02 22:34:29 +0900

    chore: remark remove

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -37,18 +37,6 @@ export default function RootLayout({
           }}
         />
       </head>
-      {/* <Script
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `
-          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-          })(window,document,'script','dataLayer','GTM-M3JNCJW')
-          `,
-        }}
-      /> */}
       <body>
         <ThemeProvider>
           <Nav />
