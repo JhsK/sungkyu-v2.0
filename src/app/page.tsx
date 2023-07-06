aa62a714df45b79863bb94c81f5292fb4d9d2f37
JhsK <qwe6293@nate.com>
2023-07-06 23:21:50 +0900

    feat: common main shadow class define & semantic tag change

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,14 +9,22 @@ export default function HomePage() {
   const posts = getPostsList({ limit: 5, category: 'all' });
 
   return (
-    <div className="px-[72px] pt-16">
+    <main className="pt-16 flex flex-col gap-9">
       {/* <Banner />
       <div className="max-w-3xl mx-auto">
         <Category activeCategory="all" />
         <List posts={posts} />
         <div className="pb-6" />
       </div> */}
-      <div className="h-[116px] rounded-2xl bg-white shadow-main"></div>
-    </div>
+      <section className="h-[116px] rounded-2xl bg-white main-shadow px-9 flex items-center justify-between">
+        <h2 className="m-0 text-3xl font-bold">내가 개발자가 될 상인가.</h2>
+        <span>아이콘 슬롯입니다</span>
+      </section>
+      <section className="flex justify-between w-full">
+        <div className="w-[calc(50%-18px)] bg-red-300 h-10"></div>
+        <div className="w-[calc(50%-18px)] bg-red-300 h-[582px] rounded-2xl main-shadow"></div>
+        {/* <div className="w-[calc(50% - 18px)] bg-red-300 h-10"></div> */}
+      </section>
+    </main>
   );
 }
