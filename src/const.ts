f5aea27e3b3ba735c1cd694e6176ecff8f88e98b
JhsK <qwe6293@nate.com>
2023-07-19 23:52:12 +0900

    feat: main page intro text add & mail url add

--- src/const.ts
+++ src/const.ts
@@ -3,10 +3,12 @@ const LINKEDIN_PROFILE =
   'https://www.linkedin.com/in/%EC%84%B1%EA%B7%9C-%EC%9E%84-5b2b8b23a/';
 const FACEBOOK_PROFILE =
   'https://www.facebook.com/profile.php?id=100006485132348';
+const MAIL_PROFILE = 'mailto:qwe6293@nate.com';
 
 export const PROFILE_URL: { [key: string]: string } = {
   github: GITHUB_PROFILE,
   linkedIn: LINKEDIN_PROFILE,
+  gmail: MAIL_PROFILE,
   facebook: FACEBOOK_PROFILE,
   twitter: GITHUB_PROFILE,
 };
@@ -23,3 +25,16 @@ export const FOOTER_ICONS = {
   facebook: 'facebook',
   github: 'github',
 };
+
+const MAIN_INTRO_TEXT = [
+  '내가 개발자가 될 상인가.',
+  '이번주까지 가능하죠? - 안돼요...',
+  '개발자가 되었다 - 어쩌다가..?',
+  '어떤 개발자가 되고 싶니?',
+  '개발자로 살아남기',
+  '먹고 살기 힘들다 - 개발자편',
+  '공부는 개발자의 숙명인가',
+  '좋은 개발자는 뭘까 - 아버지! 정답을 알려줘',
+  '안돼네... - 왜?',
+  '잘되네... - 왜?',
+];
