5e24afb7229348cee9717e85dbf470c4438346f0
JhsK <qwe6293@nate.com>
2023-07-24 23:59:28 +0900

    feat: post list article markup

--- src/app/post/page.tsx
+++ src/app/post/page.tsx
@@ -2,11 +2,14 @@ import React from 'react';
 
 const PostListPage = () => {
   return (
-    <div>
+    <div className="space-y-[104px]">
       <h3 className="pt-16 flex flex-col">
         <em className="mb-5">1년차 개발자의 고민거리</em>
         <span>개발자의 고민거리</span>
       </h3>
+      <article>
+        <div className="flex items-center justify-between">test</div>
+      </article>
     </div>
   );
 };
