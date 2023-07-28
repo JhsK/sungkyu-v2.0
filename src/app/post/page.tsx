67dbe7b4828d01ed015a90bccdeda860815a93ef
JhsK <qwe6293@nate.com>
2023-07-28 23:36:35 +0900

    feat: post list category wrapper ui

--- src/app/post/page.tsx
+++ src/app/post/page.tsx
@@ -1,4 +1,5 @@
 import CategoryBadge from '@/components/Post/CategoryBadge';
+import PostItem from '@/components/Post/PostItem';
 import React from 'react';
 
 const mockCategory = ['프론트엔드', '디자인', '알고리즘', '개인'];
@@ -10,15 +11,22 @@ const PostListPage = () => {
         <em className="mb-5">1년차 개발자의 고민거리</em>
         <span>개발자의 고민거리</span>
       </h3>
-      <article>
+      <article className="flex flex-col space-y-10">
         <div className="flex items-center justify-between">
-          <div>
+          <div className="space-x-3">
             {mockCategory.map((category) => (
               <CategoryBadge key={category}>{category}</CategoryBadge>
             ))}
           </div>
           <span>드롭다운</span>
         </div>
+        {/* <div>{[1,2,3,4,5,6,7,8,9].map((post) => (
+          <PostItem
+          post={postList[0]}
+          style={{ content: 'w-full h-[195px]' }}
+          key={post}
+        />
+        ))}</div> */}
       </article>
     </div>
   );
