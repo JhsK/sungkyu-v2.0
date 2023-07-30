658103804d59f32d7efe13a726714c39bb682e3a
JhsK <qwe6293@nate.com>
2023-07-30 17:38:31 +0900

    feat: post list & pagination ui

--- src/app/post/page.tsx
+++ src/app/post/page.tsx
@@ -1,15 +1,21 @@
+import Pagination from '@/components/Pagination';
 import CategoryBadge from '@/components/Post/CategoryBadge';
 import PostItem from '@/components/Post/PostItem';
+import { getPostsList } from '@/lib/markdown';
 import React from 'react';
 
 const mockCategory = ['프론트엔드', '디자인', '알고리즘', '개인'];
 
 const PostListPage = () => {
+  const postList = getPostsList({ limit: 8, category: 'all' });
+
   return (
     <div className="space-y-[104px]">
       <h3 className="pt-16 flex flex-col">
-        <em className="mb-5">1년차 개발자의 고민거리</em>
-        <span>개발자의 고민거리</span>
+        <em className="mb-5 not-italic text-5xl font-bold">
+          1년차 개발자의 고민거리
+        </em>
+        <span className="text-4xl">개발자의 고민거리</span>
       </h3>
       <article className="flex flex-col space-y-10">
         <div className="flex items-center justify-between">
@@ -20,14 +26,25 @@ const PostListPage = () => {
           </div>
           <span>드롭다운</span>
         </div>
-        {/* <div>{[1,2,3,4,5,6,7,8,9].map((post) => (
-          <PostItem
-          post={postList[0]}
-          style={{ content: 'w-full h-[195px]' }}
-          key={post}
-        />
-        ))}</div> */}
+        <div className="flex items-center flex-wrap gap-[5%]">
+          {postList.map((post) => (
+            <PostItem
+              post={post}
+              imageView
+              style={{ content: 'w-[408px] h-[224px]', image: 'h-[228px]' }}
+              key={post.id}
+            />
+          ))}
+        </div>
       </article>
+      <div className="mt-24">
+        <Pagination
+          center
+          currentPage={1}
+          totalPage={2}
+          onPageChange={() => {}}
+        />
+      </div>
     </div>
   );
 };
---
+++ src/components/Pagination.tsx
@@ -0,0 +1,39 @@
+interface PaginationProps {
+  currentPage: number;
+  totalPage: number;
+  center?: boolean;
+  onPageChange: (page: number) => void;
+}
+
+const Pagination = ({
+  center,
+  currentPage,
+  totalPage,
+  onPageChange,
+}: PaginationProps) => {
+  const pageArray = Array.from({ length: totalPage }, (_, index) => index + 1);
+
+  return (
+    <div className={`w-full flex ${center && 'justify-center'}`}>
+      <ul className="inline-flex -space-x-px text-sm">
+        {pageArray.map((page, index) => (
+          <li>
+            <a
+              // onClick={() => onPageChange(page)}
+              href="#"
+              className={`flex items-center justify-center px-3 h-8 text-sm font-medium ${
+                currentPage === index + 1
+                  ? 'bg-[#EAF5FF] text-[#0873FF]'
+                  : 'bg-white text-gray-500 hover:bg-gray-100 hover:text-gray-700'
+              } rounded-lg`}
+            >
+              {page}
+            </a>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default Pagination;
