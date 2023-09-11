84f1d2b6e64de1dbf67562e69e41531b482e9625
JhsK <qwe6293@nate.com>
2023-09-12 08:37:59 +0900

    v0.0.19

--- script/deploy.sh
+++ script/deploy.sh
@@ -1,14 +1,22 @@
 # 현재 버전을 가져옵니다.
+cd /Users/sungkyu/jhsk/sungkyu-v2.0
 current_version=$(jq -r '.version' package.json)
 
-# 버전 번호를 1 증가시킵니다.
-new_version=$(npm version "$current_version" --no-git-tag-version)
+# 현재 버전을 .을 기준으로 분리합니다.
+IFS='.' read -ra version_parts <<< "$current_version"
+
+# 마지막 숫자를 1 증가시킵니다.
+new_last_part=$((version_parts[2] + 1))
+
+# 새로운 버전을 생성합니다.
+new_version="${version_parts[0]}.${version_parts[1]}.$new_last_part"
 
 # package.json 파일의 버전 번호를 업데이트합니다.
 jq ".version = \"$new_version\"" package.json > tmp.json && mv tmp.json package.json
 
 # 변경 사항을 커밋합니다.
-git add package.json
+git add .
 git commit -m "v$new_version"
+git push origin main
 git tag "v$new_version"
 git push --tag
