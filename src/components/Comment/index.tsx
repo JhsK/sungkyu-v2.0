64902c6d6b87a8d006bf4dcbea0884fe9c0b548b
Jhsk <qwe6293@nate.com>
2023-12-28 13:01:25 +0900

    feat: create comment component

---
+++ src/components/Comment/index.tsx
@@ -0,0 +1,46 @@
+import { useEffect, useRef } from "react";
+
+const { NEXT_PUBLIC_GISCUS_REPO_ID, NEXT_PUBLIC_GISCUS_CATEGORY_ID } =
+  process.env;
+
+function Comment() {
+  const ref = useRef<HTMLDivElement>(null);
+  const theme = "preferred_color_scheme";
+
+  useEffect(() => {
+    if (!ref.current || ref.current.hasChildNodes()) return;
+    const scriptElem = document.createElement("script");
+    scriptElem.src = "https://giscus.app/client.js";
+    scriptElem.async = true;
+    scriptElem.crossOrigin = "anonymous";
+    scriptElem.setAttribute("data-repo", "Jhsk/sungkyu-v2.0");
+    scriptElem.setAttribute("data-repo-id", NEXT_PUBLIC_GISCUS_REPO_ID!);
+    scriptElem.setAttribute("data-category", "General");
+    scriptElem.setAttribute(
+      "data-category-id",
+      NEXT_PUBLIC_GISCUS_CATEGORY_ID!
+    );
+    scriptElem.setAttribute("data-mapping", "pathname");
+    scriptElem.setAttribute("data-strict", "0");
+    scriptElem.setAttribute("data-reactions-enabled", "1");
+    scriptElem.setAttribute("data-emit-metadata", "0");
+    scriptElem.setAttribute("data-input-position", "bottom");
+    scriptElem.setAttribute("data-theme", theme);
+    scriptElem.setAttribute("data-lang", "ko");
+    ref.current.appendChild(scriptElem);
+  }, []);
+
+  useEffect(() => {
+    const iframe = document.querySelector<HTMLIFrameElement>(
+      "iframe.giscus-frame"
+    );
+    iframe?.contentWindow?.postMessage(
+      { giscus: { setConfig: { theme } } },
+      "https://giscus.app"
+    );
+  }, [theme]);
+
+  return <section ref={ref} />;
+}
+
+export default Comment;
