a25adbaae41016390db888607fce338b630b31a2
JhsK <qwe6293@nate.com>
2024-01-05 21:46:52 +0900

    feat: typography responsive font size update

--- src/components/ui/text.tsx
+++ src/components/ui/text.tsx
@@ -6,13 +6,13 @@ interface ITextProps extends HTMLAttributes<HTMLElement> {
 }
 
 const variantStyle = {
-  h1: "text-4xl font-extrabold tracking-tight lg:text-5xl",
-  h2: "pb-2 text-3xl font-semibold tracking-tight",
-  h3: "text-2xl font-semibold tracking-tight",
-  h4: "text-xl font-semibold tracking-tight",
-  p: "leading-7",
-  div: "text-lg font-semibold",
-  small: "text-sm font-medium leading-none",
+  h1: "sm:text-4xl text-2xl font-extrabold tracking-tight lg:text-5xl",
+  h2: "pb-2 sm:text-3xl text-xl font-semibold tracking-tight",
+  h3: "sm:text-2xl text-lg font-semibold tracking-tight",
+  h4: "sm:text-xl text-base font-semibold tracking-tight",
+  p: "leading-7 sm:text-base text-sm",
+  div: "sm:text-lg text-base font-semibold",
+  small: "sm:text-sm text-xs font-medium leading-none",
 };
 
 function Text({ variant, children, className, ...props }: ITextProps) {
