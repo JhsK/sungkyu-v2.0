1c773c616fe05ca42846e56f35e63bfc11c961db
JhsK <qwe6293@nate.com>
2024-01-07 21:42:11 +0900

    feat: add BREACK_POINT and change sns name rule to scream snake case

--- src/constant/index.ts
+++ src/constant/index.ts
@@ -1,5 +1,13 @@
-export const sns = {
+export const SNS = {
   github: "https://github.com/JhsK",
   linedIn: "https://www.linkedin.com/in/sungkyu-lim-5b2b8b23a/",
   email: "mailto:qwe6293@nate.com",
+};
+
+export const BREACK_POINT = {
+  sm: 640,
+  md: 768,
+  lg: 1024,
+  xl: 1280,
+  "2xl": 1536,
 };
