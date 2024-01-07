cd7c2b7e2bec021170014e0c10ce520c35855d0a
JhsK <qwe6293@nate.com>
2024-01-07 21:18:45 +0900

    feat: responsive - category container margin

--- src/components/Posts/Categories.tsx
+++ src/components/Posts/Categories.tsx
@@ -6,7 +6,7 @@ interface ICategoriesProps {
 
 function Categories({ categories }: ICategoriesProps) {
   return (
-    <div className="flex items-center gap-4 mb-10">
+    <div className="flex items-center gap-4 sm:mb-10 mb-6">
       {categories.map((category) => (
         <Button key={category} variant="outline" className="rounded-3xl">
           {category}
