f6d43a347e3488f53565a4e68bafbff0af88324e
Jhsk <qwe6293@nate.com>
2024-02-22 16:59:04 +0900

    refactor: text component style cva apply and cn util function apply

--- src/components/ui/text.tsx
+++ src/components/ui/text.tsx
@@ -1,3 +1,5 @@
+import { cn } from "@/libs/utils";
+import { cva } from "class-variance-authority";
 import { HTMLAttributes } from "react";
 
 interface ITextProps extends HTMLAttributes<HTMLElement> {
@@ -5,24 +7,25 @@ interface ITextProps extends HTMLAttributes<HTMLElement> {
   children: React.ReactNode;
 }
 
-const variantStyle = {
-  h1: "sm:text-4xl text-2xl font-extrabold tracking-tight lg:text-5xl",
-  h2: "pb-2 sm:text-3xl text-xl font-semibold tracking-tight",
-  h3: "sm:text-2xl text-lg font-semibold tracking-tight",
-  h4: "sm:text-xl text-base font-semibold tracking-tight",
-  p: "leading-7 sm:text-base text-sm",
-  div: "sm:text-lg text-base font-semibold",
-  small: "sm:text-sm text-xs font-medium leading-none",
-};
+const variantStyle = cva("", {
+  variants: {
+    Element: {
+      h1: "sm:text-4xl text-2xl font-extrabold tracking-tight lg:text-5xl",
+      h2: "pb-2 sm:text-3xl text-xl font-semibold tracking-tight",
+      h3: "sm:text-2xl text-lg font-semibold tracking-tight",
+      h4: "sm:text-xl text-base font-semibold tracking-tight",
+      p: "leading-7 sm:text-base text-sm",
+      div: "sm:text-lg text-base font-semibold",
+      small: "sm:text-sm text-xs font-medium leading-none",
+    },
+  },
+});
 
 function Text({ variant, children, className, ...props }: ITextProps) {
   const Element = variant === "large" ? "div" : variant;
 
   return (
-    <Element
-      {...props}
-      className={`${variantStyle[Element]} ${className || ""}`}
-    >
+    <Element {...props} className={cn(variantStyle({ Element }), className)}>
       {children}
     </Element>
   );
