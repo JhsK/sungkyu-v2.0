71e5e94d6d658b0c9cb64b7f5d6995e2757e6c29
JhsK <qwe6293@nate.com>
2024-02-24 22:17:05 +0900

    feat: add hero section information component add

---
+++ src/components/Info/index.tsx
@@ -0,0 +1,27 @@
+import Text from "../ui/text";
+
+function Info() {
+  return (
+    <div className="pt-4 flex flex-col gap-1.5">
+      <Text variant="h1" className="font-extralight">
+        안녕하세요!
+      </Text>
+      <Text variant="h1" className="font-extralight">
+        복잡함을 단순함으로 바꾸는
+      </Text>
+      <div className="flex">
+        <Text variant="h1" className="font-extralight mr-2">
+          개발자
+        </Text>
+        <Text variant="h1" className="font-bold">
+          임성규
+        </Text>
+        <Text variant="h1" className="font-extralight">
+          입니다.
+        </Text>
+      </div>
+    </div>
+  );
+}
+
+export default Info;
--- src/components/Posts/Hero2.tsx
+++ src/components/Posts/Hero2.tsx
@@ -1,19 +1,13 @@
-import Test from "../Test";
-import Text from "../ui/text";
+import DroppingImageBlocks from "../DroppingImageBlocks";
+import Info from "../Info";
 
 function Hero2() {
   return (
-    <div className="flex sm:flex-row items-center flex-col">
-      <div className="pt-4">
-        <Text variant="h1" className="sm:mb-4">
-          About
-        </Text>
-        <Text variant="p">
-          안녕하세요 주니어 프론트엔드 개발자 임성규입니다. <br />
-          스타트업에 근무하여 주도적이며 지속가능한 코드에 대해 고민합니다.
-        </Text>
+    <div className="relative flex sm:flex-row items-center flex-col">
+      <DroppingImageBlocks />
+      <div className="absolute top-[45%] left-[3%] -translate-y-1/2">
+        <Info />
       </div>
-      <Test />
     </div>
   );
 }
