409d6f3677b4a3d5ec5b650283b439a05bd296f5
JhsK <qwe6293@nate.com>
2024-02-25 14:48:53 +0900

    feat: DroppingImageBlocks dynamic import and add loading

---
+++ src/components/DroppingImageBlocks/Loading.tsx
@@ -0,0 +1,5 @@
+function DroppingImageBlocksLoading() {
+  return <div className="w-full h-[500px] bg-[#F0F0F0]"></div>;
+}
+
+export default DroppingImageBlocksLoading;
--- src/components/Posts/Hero2.tsx
+++ src/components/Posts/Hero2.tsx
@@ -1,11 +1,17 @@
-import DroppingImageBlocks from "../DroppingImageBlocks";
+import dynamic from "next/dynamic";
 import Info from "../Info";
+import DroppingImageBlocksLoading from "../DroppingImageBlocks/Loading";
+
+const DroppingImageBlocks = dynamic(() => import("../DroppingImageBlocks"), {
+  loading: () => <DroppingImageBlocksLoading />,
+  ssr: false,
+});
 
 function Hero2() {
   return (
     <div className="relative flex sm:flex-row items-center flex-col">
       <DroppingImageBlocks />
-      <div className="absolute top-[45%] left-[2%] -translate-y-1/2">
+      <div className="absolute top-[45%] left-[6%] -translate-y-1/2">
         <Info />
       </div>
     </div>
