12a9811e238c9d4d639c2231c79f1b7d15e3511a
JhsK <qwe6293@nate.com>
2024-03-02 17:54:48 +0900

    fix: main infromation text chage image and position

--- src/components/Info/index.tsx
+++ src/components/Info/index.tsx
@@ -1,26 +1,14 @@
-import Text from "../ui/text";
+import Image from "next/image";
+import InfoImage from "../../../public/main.png";
 
 function Info() {
   return (
-    <div className="pt-4 flex flex-col gap-1.5">
-      <Text variant="h1" className="font-extralight">
-        안녕하세요!
-      </Text>
-      <Text variant="h1" className="font-extralight">
-        복잡함을 단순함으로 바꾸는
-      </Text>
-      <div className="flex">
-        <Text variant="h1" className="font-extralight mr-2">
-          개발자
-        </Text>
-        <Text variant="h1" className="font-normal">
-          임성규
-        </Text>
-        <Text variant="h1" className="font-extralight">
-          입니다.
-        </Text>
-      </div>
-    </div>
+    <Image
+      src={InfoImage}
+      width={450}
+      height={200}
+      alt="sungkyu's blog info text"
+    />
   );
 }
 
