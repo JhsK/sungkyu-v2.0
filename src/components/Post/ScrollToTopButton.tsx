ee76e59d1479fa5beed910196b5ee24416028aea
JhsK <qwe6293@nate.com>
2024-03-02 18:32:41 +0900

    feat: creating a scroll move button component using react-lottie

---
+++ src/components/Post/ScrollToTopButton.tsx
@@ -0,0 +1,56 @@
+import { cn } from "@/libs/utils";
+import { useEffect, useState } from "react";
+import Lottie from "react-lottie";
+import config from "../../../public/lottie/scrollToTopButton.json";
+
+const defaultOptions = {
+  loop: true,
+  autoplay: true,
+  animationData: config,
+  rendererSettings: {
+    preserveAspectRatio: "xMidYMid slice",
+  },
+};
+
+function ScrollToTopButton() {
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  useEffect(() => {
+    if (window !== undefined) {
+      const handleIsScroll = () => {
+        setIsScrolled(window.scrollY > 150);
+      };
+
+      window.addEventListener("scroll", handleIsScroll);
+
+      return () => window.removeEventListener("scroll", handleIsScroll);
+    }
+  }, []);
+
+  const handleClickButton = () => {
+    if (window !== undefined) {
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+    }
+  };
+
+  return (
+    <div
+      className={cn(
+        "fixed bottom-[3%] right-12 opacity-0 transition-opacity",
+        isScrolled && "opacity-100"
+      )}
+    >
+      <div
+        className="rotate-180 flex items-center justify-center"
+        onClick={handleClickButton}
+      >
+        <Lottie options={defaultOptions} width={80} height={80} />
+      </div>
+    </div>
+  );
+}
+
+export default ScrollToTopButton;
