58127190214617f599baa000580a822d4eb16817
JhsK <qwe6293@nate.com>
2024-03-04 21:49:15 +0900

    feat: DroppingImageBlocksLoading apply lottie and delay

--- src/components/DroppingImageBlocks/Loading.tsx
+++ src/components/DroppingImageBlocks/Loading.tsx
@@ -1,5 +1,40 @@
+import Lottie from "react-lottie";
+import config from "../../../public/lottie/loading.json";
+import { useEffect, useState } from "react";
+
+const defaultOptions = {
+  loop: true,
+  autoplay: true,
+  animationData: config,
+  rendererSettings: {
+    preserveAspectRatio: "xMidYMid slice",
+  },
+};
+
+function Delay({ children }: { children: React.ReactNode }) {
+  const [isDelayed, setIsDelayed] = useState(false);
+
+  useEffect(() => {
+    const timeoutId = setTimeout(() => {
+      setIsDelayed(true);
+    }, 200);
+
+    return () => clearTimeout(timeoutId);
+  }, []);
+
+  if (!isDelayed) return;
+
+  return children;
+}
+
 function DroppingImageBlocksLoading() {
-  return <div className="w-full h-[500px] bg-white"></div>;
+  return (
+    <div className="w-full h-[500px] bg-white flex items-center justify-center">
+      <Delay>
+        <Lottie options={defaultOptions} width={120} height={120} />
+      </Delay>
+    </div>
+  );
 }
 
 export default DroppingImageBlocksLoading;
