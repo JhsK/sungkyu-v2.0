cb21b67628d2d58dbb7e70d093f85f928329b137
JhsK <qwe6293@nate.com>
2024-03-06 21:43:04 +0900

    fix: remove next third party package & inline ga script

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,14 +1,30 @@
 import { Html, Head, Main, NextScript } from "next/document";
-import { GoogleAnalytics } from "@next/third-parties/google";
+
+const GA_Measurement_ID = process.env.NEXT_PUBLIC_MEASUREMENT_ID;
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <script
+          async
+          src={`https://www.googletagmanager.com/gtag/js?id=${GA_Measurement_ID}`}
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+          
+            gtag('config', '${GA_Measurement_ID}');
+        `,
+          }}
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
-        <GoogleAnalytics gaId="G-18E8JCMEEQ" />
       </body>
     </Html>
   );
