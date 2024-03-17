6c61809854c9a362e82ef161277f8555573449a5
JhsK <qwe6293@nate.com>
2024-03-17 17:07:43 +0900

    fix: giscus 404 error

--- src/components/Comment/index.tsx
+++ src/components/Comment/index.tsx
@@ -1,46 +1,21 @@
-import { useEffect, useRef } from "react";
+import Giscus from "@giscus/react";
 
 function Comment() {
-  const ref = useRef<HTMLDivElement>(null);
-  const theme = "preferred_color_scheme";
-
-  useEffect(() => {
-    if (!ref.current || ref.current.hasChildNodes()) return;
-    const scriptElem = document.createElement("script");
-    scriptElem.src = "https://giscus.app/client.js";
-    scriptElem.async = true;
-    scriptElem.crossOrigin = "anonymous";
-    scriptElem.setAttribute("data-repo", "Jhsk/sungkyu-v2.0");
-    scriptElem.setAttribute(
-      "data-repo-id",
-      process.env.NEXT_PUBLIC_GISCUS_REPO_ID!
-    );
-    scriptElem.setAttribute("data-category", "General");
-    scriptElem.setAttribute(
-      "data-category-id",
-      process.env.NEXT_PUBLIC_GISCUS_CATEGORY_ID!
-    );
-    scriptElem.setAttribute("data-mapping", "pathname");
-    scriptElem.setAttribute("data-strict", "0");
-    scriptElem.setAttribute("data-reactions-enabled", "1");
-    scriptElem.setAttribute("data-emit-metadata", "0");
-    scriptElem.setAttribute("data-input-position", "bottom");
-    scriptElem.setAttribute("data-theme", theme);
-    scriptElem.setAttribute("data-lang", "ko");
-    ref.current.appendChild(scriptElem);
-  }, []);
-
-  useEffect(() => {
-    const iframe = document.querySelector<HTMLIFrameElement>(
-      "iframe.giscus-frame"
-    );
-    iframe?.contentWindow?.postMessage(
-      { giscus: { setConfig: { theme } } },
-      "https://giscus.app"
-    );
-  }, [theme]);
-
-  return <section ref={ref} />;
+  return (
+    <Giscus
+      id="comments"
+      repo="Jhsk/sungkyu-v2.0"
+      repoId="R_kgDOI9uP_g"
+      category="General"
+      categoryId="DIC_kwDOI9uP_s4CcA1L"
+      mapping="pathname"
+      reactionsEnabled="1"
+      emitMetadata="0"
+      inputPosition="bottom"
+      theme="preferred_color_scheme"
+      lang="ko"
+    />
+  );
 }
 
 export default Comment;
