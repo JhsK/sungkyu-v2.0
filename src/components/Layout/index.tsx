66430019e7d81763507ee0bd4655ce4cc79acdc8
Jhsk <qwe6293@nate.com>
2024-07-16 16:51:38 +0900

    feat: Layout type에 따른 여백 스타일 구조 변경

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,18 +1,32 @@
+import { cva } from "class-variance-authority";
 import Footer from "../Footer";
 import Header from "../Header";
 
 interface ILayoutProps {
   children: React.ReactNode;
+  type: "spread" | "centered";
 }
 
-function Layout({ children }: ILayoutProps) {
+function Layout({ children, type = "centered" }: ILayoutProps) {
   return (
-    <div className="lg:max-w-[1024px] md:max-w-3xl max-w-[calc(100vw-48px)] mx-auto h-screen flex flex-col">
+    <div className={LayoutVariants({ type })}>
       <Header />
-      <div className="flex-1">{children}</div>
+      <div className="flex-1 lg:max-w-[1024px] md:max-w-3xl max-w-[calc(100vw-48px)] mx-auto">
+        {children}
+      </div>
       <Footer />
     </div>
   );
 }
+
+const LayoutVariants = cva("", {
+  variants: {
+    type: {
+      spread: "h-screen flex flex-col items-cetner px-10",
+      centered:
+        "lg:max-w-[1024px] md:max-w-3xl max-w-[calc(100vw-48px)] mx-auto h-screen flex flex-col",
+    },
+  },
+});
 
 export default Layout;
